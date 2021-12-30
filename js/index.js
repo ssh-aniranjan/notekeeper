3fa5a07935172e75d27cd3c8fa057d0f74b85997
TheGhostCoderIN <theghostcoder777@gmail.com>
2021-12-30 11:06:48 +0530

    basic project complete

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const clearBtn = document.querySelectorAll('.textAreaBtn > button')[0];
+//clear
+
+const addNoteBtn = document.querySelectorAll('.textAreaBtn >button')[1];
+//add note
+
+const clearAllBtn = document.querySelectorAll('.notesHeader > button')[0];
+//clearallbtn
+
+
+
+console.log(clearBtn);
+console.log(clearAllBtn);
+console.log(addNoteBtn)
