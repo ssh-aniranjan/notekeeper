72ea893cf3390ad9a1a92aa09d69834aca16c052
aniranjan2001 <aniranjan53@gmail.com>
2021-12-31 02:22:39 +0530

    Added location feature

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+const noteBox = document.querySelector('#notebox');
+const locationBox = document.querySelector('header > p');
+const notesContainer = document.querySelector('#notesContainer')
 const clearBtn = document.querySelectorAll('.textAreaBtn > button')[0];
 //clear
 
@@ -7,8 +10,66 @@ const addNoteBtn = document.querySelectorAll('.textAreaBtn >button')[1];
 const clearAllBtn = document.querySelectorAll('.notesHeader > button')[0];
 //clearallbtn
 
+addNoteBtn.addEventListener('click', () => {
+    let div = document.createElement('div');
+    div.setAttribute(className, 'notes');
+    let h3 = document.createElement('h3');
+    h3.innerText = "Note";
+    div.appendChild(h3);
+    let p = document.createElement('p');
+    p.innerText = 'Helllpp ksgbakjdfbhd';
+    div.appendChild(p);
+    let button = document.createElement('button');
+    button.innerText = 'Delete Note';
+    div.appendChild(p);
+    notesContainer.appendChild(div);
+    var dataString = localStorage.getItem('noteKeeperString');
+    if (dataString) {
+        console.log(JSON.parse(dataString));
+    }
+    else {
+        console.log(dataString);
+    }
+    window.alert("Added note");
+})
 
+clearAllBtn.addEventListener('click', () => {
+    let deleteAllResponse = window.confirm("Do you want to delete All Notes");
+    if (deleteAllResponse) {
+        console.log("Deleted all the notes");
+    }
+})
 
-console.log(clearBtn);
-console.log(clearAllBtn);
-console.log(addNoteBtn)
+window.addEventListener('load', () => {
+
+    if (window.navigator.geolocation) {
+        window.navigator.geolocation.getCurrentPosition((res) => {
+            showPosition(res.coords.longitude,res.coords.latitude);
+        }, positionError);
+        // Geolocation available
+    }
+});
+function showPosition(long,lat) {
+    // const { latitude, longitude } = location.coords;
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=36e300b1a8814eaa85e8fd24d413fb30`)
+        .then(response => response.json())
+        .then(data => {
+            locationBox.innerText = `${data.results[0].components.city},${data.results[0].components.state}`;
+            
+            // console.log(data.results[0].components);
+            // console.log("Hello");
+        })
+        .catch(err => console.log("No dtata retrieved"));
+
+    // https://api.opencagedata.com/geocode?/v1/json?q=LAT+LNG&key=36e300b1a8814eaa85e8fd24d413fb30
+};
+
+function positionError() {
+    console.log("none");
+}
+
+// function noCords();
+
+// console.log(clearBtn);
+// console.log(clearAllBtn);
+// console.log(addNoteBtn)
