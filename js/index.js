d5bc03cad212af52e951485b2337dd2e8864fa70
aniranjan2001 <aniranjan53@gmail.com>
2022-01-03 10:11:27 +0530

    script optimization.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 const noteBox = document.querySelector('#notebox');
 const locationBox = document.querySelector('header > p');
 const locationCity = document.querySelectorAll('header > p >span')[0];
-// const locationState = document.querySelectorAll('header > p >span')[1];
 const notesContainer = document.querySelector('#notesContainer');
 const clearBtn = document.querySelectorAll('.textAreaBtn > button')[0];
 //clear
@@ -13,7 +12,6 @@ const clearAllBtn = document.querySelectorAll('.notesHeader > button')[0];
 //clearallbtn
 const deleteBtns = document.getElementsByClassName('deleteBtn');
 console.log(deleteBtns);
-// var notesArray = localStorage.getItem('notesKeeperString');
 
 //clear noteBox
 clearBtn.addEventListener('click', function () {
@@ -33,7 +31,6 @@ clearAllBtn.addEventListener('click', function () {
 addNoteBtn.addEventListener('click', () => {
     if (noteBox.value) {
         if (localStorage.getItem('notesKeeperString')) {
-            // Window.alert("Something is here");
             var allNotes = localStorage.getItem('notesKeeperString');
             notesArray = [];
             notesArray = JSON.parse(allNotes);
@@ -47,7 +44,6 @@ addNoteBtn.addEventListener('click', () => {
                                                 <button class="deleteBtn" id=${index}>Delete Note</button>`
                     divElement.className = "notes";
                     notesContainer.appendChild(divElement);
-                    // window.alert("Inner Loop ran");
 
                 });
             }
@@ -57,7 +53,6 @@ addNoteBtn.addEventListener('click', () => {
         }
 
         else {
-            // window.alert("Nothing is here");
             notesArray = [];
             notesArray.push(`${noteBox.value}`);
             if (notesArray.length) {
@@ -76,27 +71,7 @@ addNoteBtn.addEventListener('click', () => {
             allNotes = JSON.stringify(notesArray);
             localStorage.setItem('notesKeeperString', allNotes);
         }
-        // console.log(notesArray);
         noteBox.value = "";
-
-
-        // let divElement = document.createElement('div');
-        // divElement.innerHTML = `<h3>Note</h3>
-        // <p>${noteBox.value}</p>
-        // <button>Delete Note</button>`
-        // divElement.className = "notes";
-        // notesContainer.appendChild(divElement);
-
-        // if(localStorage.getItem('notesKeeperString')){
-        //     allNotes = localStorage.getItem('notesKeeperString');
-        //     console.log(allNotes);
-
-        // }
-        // var notesArray = new Array;
-        // notesArray = JSON.parse(allNotes);
-        // notesArray.push(`${noteBox.value}`);
-        // allNotes = JSON.stringify(notesArray);
-        // localStorage.setItem('notesKeeperString',allNotes);
     }
     else {
         window.alert("Nothing to add");
@@ -106,14 +81,12 @@ addNoteBtn.addEventListener('click', () => {
 // //delete button for indivisual note
 window.addEventListener('click', function (event) {
     if (typeof (event.target.id) === "number" || event.target.className === "deleteBtn") {
-        // window.alert("Delete Btn clicked");
         if (localStorage.getItem('notesKeeperString') !== "") {
 
             allNotes = localStorage.getItem('notesKeeperString');
             notesArray = [];
             notesArray = JSON.parse(allNotes);
             notesArray.splice(event.target.id, 1);
-            // window.alert("deleting note " + parseInt(event.target.id)+1);
             notesContainer.innerHTML = ``;
             if (notesArray.length) {
                 notesArray.forEach((element, index) => {
@@ -123,7 +96,6 @@ window.addEventListener('click', function (event) {
                                             <button class="deleteBtn" id=${index}>Delete Note</button>`
                     divElement.className = "notes";
                     notesContainer.appendChild(divElement);
-                    // window.alert("Inner Loop ran");
 
                 });
             }
@@ -141,18 +113,15 @@ window.addEventListener('click', function (event) {
 
     }
 
-    //     console.log(event.target.id);
 });
 
 //fetching notes from local storage
 window.addEventListener('load', function () {
     if (localStorage.getItem('notesKeeperString')) {
-        // window.alert("Loop runned");
         var allNotes = localStorage.getItem('notesKeeperString');
         if (typeof (allNotes) === 'string') {
-            notesArray =[];
+            notesArray = [];
             notesArray = JSON.parse(allNotes);
-            // notesContainer.innerHTML = `<h2>Loop runned till here...</h2>`;
             if (notesArray.length) {
                 notesArray.forEach((element, index) => {
                     let divElement = document.createElement('div');
@@ -175,7 +144,7 @@ window.addEventListener('load', function () {
     else {
         notesContainer.innerHTML = `<h2>No Notes added...</h2>`;
     }
-    
+
 });
 
 //location adding code
@@ -207,7 +176,6 @@ function showPosition(long, lat) {
     fetch(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=36e300b1a8814eaa85e8fd24d413fb30`)
         .then(response => response.json())
         .then(data => {
-            // console.log(data.results);
             locationCity.innerText = `${data.results[0].components.city}, ${data.results[0].components['ISO_3166-1_alpha-2']}`;
         })
         .catch(err => console.log(err + "No dtata retrieved"));
